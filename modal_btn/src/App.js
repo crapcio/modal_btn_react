9ab04d6fa8f03cc75547191cb5fa88b63f4214e3
Your Name <you@example.com>
2024-09-05 15:07:04 +0300

    some fixes with modal window

--- modal_btn/src/App.js
+++ modal_btn/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 
 function App() {
+  const Modal = ({ open, setOpen }) => (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+      <div className="modal">
+        <svg
+          onClick={() => setOpen(false)}
+          height="200"
+          viewBox="0 0 200 200"
+          width="200"
+        >
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        <img
+          className="GIF"
+          src="https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif?cid=790b7611d44dwujil51vf1ieh1shkiwwb9u0wvoocannfp4t&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+          style={{ width: '30%', height: '20%', objectFit: 'contain' }}
+          alt="GIF"
+        />
+      </div>
+    </div>
+  );
+
   const [open, setOpen] = React.useState(false);
+
   return (
     <div className="App">
-      <button onClick={() => setOpen(true)} className="open-modal-btn">✨Открыть модальное окно</button>
-      {
-        open && (
-          <div className="overlay">
-          <div className="modal">
-            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img className='GIF' src="https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif?cid=790b7611d44dwujil51vf1ieh1shkiwwb9u0wvoocannfp4t&ep=v1_gifs_search&rid=giphy.gif&ct=g" style={{ width: '30%', height: '20%', objectFit: 'contain' }}  />
-          </div>
-        </div>
-        )
-      }
+      <button onClick={() => setOpen(true)} className="open-modal-btn">
+        ✨Открыть модальное окно
+      </button>
+      {open && <Modal open={open} setOpen={setOpen} />}
     </div>
   );
 }
